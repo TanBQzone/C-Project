2a4de1c8447e50f8203cc108dd8205b9b6814c6d
TanBQ <68118866+TanBQzone@users.noreply.github.com>
2023-05-09 13:49:26 +0800

    Update Student_info.c

--- Student_info.c
+++ Student_info.c
@@ -38,118 +38,140 @@
     - 其中，第一行为数据的表头，第一个字段为姓名，后面8个字段为各门课程的成绩。
     - 每行数据以逗号分隔，行末不要有多余的空格或逗号。
     - 文件名可以自己定义，但是需要在程序中正确设置文件路径和文件名，以确保程序可以正确读取和写入文件。
-    
-测试数据:
-    张博,83,75,84,81,88,77,86,82
-    李璐,88,90,92,85,87,89,91,83
-    陈秋,78,82,85,87,89,82,83,79
-    赵涛,91,92,93,94,95,96,97,88
-    朱文,82,80,87,89,90,88,85,83
-    孙阳,83,88,92,89,87,90,91,87
-    周娜,78,85,89,80,83,81,85,82
-    吴静,92,88,89,94,90,92,88,85
-    郑晨,85,87,91,82,80,88,90,83
-    王洁,88,91,92,87,89,90,93,85
-    张伟,82,80,87,89,90,88,85,83
-    李鹏,85,88,90,87,89,92,85,82
-    赵慧,91,93,92,94,95,96,97,88
-    钱涵,82,85,86,89,87,88,85,83
-    孙蕾,83,88,90,89,87,90,91,87
-    周俊,78,85,89,80,83,81,85,82
-    吴宇,92,88,89,94,90,92,88,85
-    郑亮,85,87,91,82,80,88,90,83
-    王峰,88,91,92,87,89,90,93,85
-    李娜,82,80,87,89,90,88,85,83
 
+测试的数据:
+    张博 83 75 84 81 88 77 86 82
+    李璐 88 90 92 85 87 89 91 83
+    陈秋 78 82 85 87 89 82 83 79
+    赵涛 91 92 93 94 95 96 97 88
+    朱文 82 80 87 89 90 88 85 83
+    孙阳 83 88 92 89 87 90 91 87
+    周娜 78 85 89 80 83 81 85 82
+    吴静 92 88 89 94 90 92 88 85
+    郑晨 85 87 91 82 80 88 90 83
+    王洁 88 91 92 87 89 90 93 85
+    张伟 82 80 87 89 90 88 85 83
+    李鹏 85 88 90 87 89 92 85 82
+    赵慧 91 93 92 94 95 96 97 88
+    钱涵 82 85 86 89 87 88 85 83
+    孙蕾 83 88 90 89 87 90 91 87
+    周俊 78 85 89 80 83 81 85 82
+    吴宇 92 88 89 94 90 92 88 85
+    郑亮 85 87 91 82 80 88 90 83
+    王峰 88 91 92 87 89 90 93 85
+    李娜 82 80 87 89 90 88 85 83
  */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-#define NUM_OF_COURSES 8        // 本学期的课程数目
-#define NUM_OF_CORE_COURSES 4   // 核心课程数目
-#define NUM_OF_PRIZE_A 2        // 获得甲等奖学金
-#define NUM_OF_PRIZE_B 3        // 获得乙等奖学金
-#define NUM_OF_PRIZE_C 5        // 获得丙等奖学金的学生人数
+#define MAX_NAME_LENGTH 10    // 最大姓名数，中文要除2
+#define NUM_OF_COURSES 8      // 本学期的课程数目
+#define NUM_OF_CORE_COURSES 4 // 核心课程数目
+#define NUM_OF_PRIZE_A 2      // 获得甲等奖学金
+#define NUM_OF_PRIZE_B 3      // 获得乙等奖学金
+#define NUM_OF_PRIZE_C 5      // 获得丙等奖学金的学生人数
 
-//定义一个结构体,去保存学生的信息
-struct student {
-    int uid;                    // 学生学号
+// 定义一个结构体,去保存学生的信息
+struct Student
+{
     char name[20];              // 学生姓名
     int scores[NUM_OF_COURSES]; // 学生所有课程的成绩
     int total_score;            // 通过'calculate_scores'函数计算出的总成绩
     float avg_score;            // 通过'calculate_scores'函数计算出的平均分
 };
 
-void display_menu();                                                    // 显示目录
-void read_scores(struct student students[], int num_of_students);       // 从文件读取成绩
-void calculate_scores(struct student students[], int num_of_students);  // 计算成绩和平均分
-void select_prize(struct student students[], int num_of_students);      // 排序和输出获奖人数
-void debug_tools(struct student student[]);                             // debug接口，用于检查数据测试
+void display_menu();                                                   // 显示目录
+void read_scores(struct Student students[], int num_of_students);      // 从文件读取成绩
+void calculate_scores(struct Student students[], int num_of_students); // 计算成绩和平均分
+void select_prize(struct Student students[], int num_of_students);     // 排序和输出获奖人数
+void debug_tools(struct Student studens[], int num_of_students);       // debug接口，用于检查数据测试
 
-int main() {
+int main()
+{
     int num_of_students;
     printf("该班级有多少名学生: ");
     scanf("%d", &num_of_students);
 
-    struct student students[num_of_students];               //构建一个结构体数组，保存全班的信息
+    struct Student students[num_of_students]; // 构建一个结构体数组，保存全班的信息
 
     int option;
-    do {
+    do
+    {
         system("cls");
-        display_menu();                                     //显示目录
+        display_menu(); // 显示目录
         printf("请选择目录: ");
         scanf("%d", &option);
-        switch (option) {
-            case 1:
-                system("cls");
-                read_scores(students, num_of_students);
-                break;
-            case 2:
-                system("cls");
-                calculate_scores(students, num_of_students);
-                break;
-            case 3:
-                system("cls");
-                select_prize(students, num_of_students);
-                break;
-            case 4:
-                system("cls");
-                printf("欢迎您的下次使用!\n");
-                break;
-            default:
-                system("cls");
-                printf("输入有误.\n");
-                break;
+        switch (option)
+        {
+        case 1:
+            system("cls");
+            read_scores(students, num_of_students);
+            // debug_tools(students, num_of_students);
+            break;
+        case 2:
+            system("cls");
+            calculate_scores(students, num_of_students);
+            break;
+        case 3:
+            system("cls");
+            select_prize(students, num_of_students);
+            break;
+        case 4:
+            system("cls");
+            printf("欢迎您的下次使用!\n");
+            break;
+        default:
+            system("cls");
+            printf("输入有误.\n");
+            break;
         }
     } while (option != 4);
 
     return 0;
 }
 
-void display_menu() {
+void display_menu()
+{
     printf("1. 从文件读取学生信息\n");
     printf("2. 计算总成绩和平均成绩\n");
     printf("3. 计算和输出获奖人数\n");
     printf("4. 退出'奖学金评选系统'\n");
 }
 
-void read_scores(struct student students[], int num_of_students) {
+void debug_tools(struct Student students[], int num_of_students)
+{
+    for (int i = 0; i < num_of_students; i++)
+    {
+        printf("%s: \n   ", students[i].name);
+        for (int j = 0; j < NUM_OF_COURSES; j++)
+        {
+            printf(" %d", students[i].scores[j]);
+        }
+        printf("\n");
+    }
+}
+
+void read_scores(struct Student students[], int num_of_students)
+{
     char filename[20];
     printf("输入文件名: ");
     scanf("%s", filename);
 
     FILE *fp;
     fp = fopen(filename, "r");
-    if (fp == NULL) {
+    if (fp == NULL)
+    {
         printf("无法打开文件或者文件不存在.\n");
         return;
     }
 
-    for (int i = 0; i < num_of_students; i++) {
+    for (int i = 0; i < num_of_students; i++)
+    {
         fscanf(fp, "%s", students[i].name);
-        for (int j = 0; j < NUM_OF_COURSES; j++) {
+        for (int j = 0; j < NUM_OF_COURSES; j++)
+        {
             fscanf(fp, "%d", &students[i].scores[j]);
         }
     }
@@ -159,32 +181,41 @@ void read_scores(struct student students[], int num_of_students) {
     printf("成功读取文件. \n");
 }
 
-void calculate_scores(struct student students[], int num_of_students) {
-    for (int i = 0; i < num_of_students; i++) {
+void calculate_scores(struct Student students[], int num_of_students)
+{
+    for (int i = 0; i < num_of_students; i++)
+    {
         int total_score = 0;
-        for (int j = 0; j < NUM_OF_COURSES; j++) {
+        for (int j = 0; j < NUM_OF_COURSES; j++)
+        {
             total_score += students[i].scores[j];
         }
         students[i].total_score = total_score;
-        students[i].avg_score = (float) total_score / NUM_OF_COURSES;
+        students[i].avg_score = (float)total_score / NUM_OF_COURSES;
     }
 
     printf("计算完成.\n");
 }
 
-void select_prize(struct student students[], int num_of_students) {
+void select_prize(struct Student students[], int num_of_students)
+{
     // 按照总分排序已有的学生
-    for (int i = 0; i < num_of_students - 1; i++) {
-        for (int j = i + 1; j < num_of_students; j++) {
-            if (students[j].total_score > students[i].total_score) {
-                struct student temp = students[i];
+    for (int i = 0; i < num_of_students - 1; i++)
+    {
+        for (int j = i + 1; j < num_of_students; j++)
+        {
+            if (students[j].total_score > students[i].total_score)
+            {
+                struct Student temp = students[i];
                 students[i] = students[j];
                 students[j] = temp;
-            } else if (students[j].total_score == students[i].total_score &&
-                       students[j].scores[NUM_OF_CORE_COURSES] > students[i].scores[NUM_OF_CORE_COURSES]) {
+            }
+            else if (students[j].total_score == students[i].total_score &&
+                     students[j].scores[NUM_OF_CORE_COURSES] > students[i].scores[NUM_OF_CORE_COURSES])
+            {
                 // 如果所有成绩一样，就看核心课程
                 // 通过循环，依次比较该学生的四门核心课程
-                struct student temp = students[i];
+                struct Student temp = students[i];
                 students[i] = students[j];
                 students[j] = temp;
             }
@@ -194,15 +225,18 @@ void select_prize(struct student students[], int num_of_students) {
     // 输出甲乙丙三类学生
     printf("下列获奖学生[学号]:[姓名] - [分数]:\n");
     printf("A:\n");
-    for (int i = 0; i < NUM_OF_PRIZE_A; i++) {
-        printf("\t%d:%s - %d\n", students[i].uid, students[i].name, students[i].total_score);
+    for (int i = 0; i < NUM_OF_PRIZE_A; i++)
+    {
+        printf("\t%s - %d\n", students[i].name, students[i].total_score);
     }
     printf("B:\n");
-    for (int i = NUM_OF_PRIZE_A; i < NUM_OF_PRIZE_A + NUM_OF_PRIZE_B; i++) {
-        printf("\t%d:%s - %d\n", students[i].uid, students[i].name, students[i].total_score);
+    for (int i = NUM_OF_PRIZE_A; i < NUM_OF_PRIZE_A + NUM_OF_PRIZE_B; i++)
+    {
+        printf("\t%s - %d\n", students[i].name, students[i].total_score);
     }
     printf("C:\n");
-    for (int i = NUM_OF_PRIZE_A + NUM_OF_PRIZE_B; i < NUM_OF_PRIZE_A + NUM_OF_PRIZE_B + NUM_OF_PRIZE_C; i++) {
-        printf("\t%d:%s - %d\n", students[i].uid, students[i].name, students[i].total_score);
+    for (int i = NUM_OF_PRIZE_A + NUM_OF_PRIZE_B; i < NUM_OF_PRIZE_A + NUM_OF_PRIZE_B + NUM_OF_PRIZE_C; i++)
+    {
+        printf("\t%s - %d\n", students[i].name, students[i].total_score);
     }
 }
